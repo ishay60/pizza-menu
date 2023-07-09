a322646f31cdafbebb5ca10d69d47788ea7a8792
Ishay Rosengarten <ishay60@gmail.com>
2023-07-09 07:00:51 +0300

    photos work

--- src/index.jsx
+++ src/index.jsx
@@ -1,34 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import pizzaData from "./data";
+import pizzaSpinaci from "./pizzas/spinaci.jpg";
 
 function App() {
-    return (
-      <div>
-        <h1>Hello React</h1>
-          <Pizza />
-          <Pizza />
-          <Pizza />
-        
-      </div>
-    );
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
 }
 
 function Pizza() {
-    return (
-      <div>
-        <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
-        <h2>Pizza Spinaci</h2>
-        <p>Tomato,mozarella,spinach,and riccota cheese</p>
-      </div>
-    );
+  return (
+    <div>
+      <img className="pizza-spinaci" src={pizzaSpinaci} alt="Pizza Spinaci" />
+      <h2>Pizza Spinaci</h2>
+      <p>Tomato,mozarella,spinach,and riccota cheese</p>
+    </div>
+  );
 }
 
 //react version 18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-  
     {/* // during development it will run twice to make sure theres no bugs. */}
     <App />
   </React.StrictMode>
@@ -36,4 +34,3 @@ root.render(
 
 //React <18 :
 //React.render(<App/>);
- 
