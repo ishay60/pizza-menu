c95520e423e4f40e26b6a0281b1622fa17447db1
Ishay Rosengarten <ishay60@gmail.com>
2023-07-09 06:41:44 +0300

    images

--- src/index.js
+++ src/index.js
@@ -1,18 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import pizzaData from "./data";
 
 function App() {
-  return <h1>Hello React</h1>;
+    return (
+      <div>
+        <h1>Hello React</h1>
+          <Pizza />
+          <Pizza />
+          <Pizza />
+        
+      </div>
+    );
 }
+
+function Pizza() {
+    return (
+      <div>
+        <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
+        <h2>Pizza Spinaci</h2>
+        <p>Tomato,mozarella,spinach,and riccota cheese</p>
+      </div>
+    );
+}
+
 //react version 18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {" "}
-    // during development it will run twice to make sure theres no bugs.
+  
+    {/* // during development it will run twice to make sure theres no bugs. */}
     <App />
   </React.StrictMode>
 );
 
 //React <18 :
 //React.render(<App/>);
+ 
